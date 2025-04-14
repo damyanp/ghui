bb965fa391af1538471c07b8a30f65c17331b7b1
Damyan Pepper <damyanp@gmail.com>
2025-04-13 21:16:35 -0700

    Use keyring to store PAT

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -10,16 +10,26 @@ enum PatStatus {
     Broken,
 }
 
-#[derive(Default)]
 struct PATState {
-    pat: String,
+    pat_entry : keyring::Entry
+}
+
+impl Default for PATState {
+    fn default() -> Self {
+        let pat_entry = keyring::Entry::new("ghui", "local user").expect("keyring failed to get entry");
+
+        println!("{:?}", pat_entry);
+
+        Self { pat_entry }
+    }
 }
 
 #[tauri::command]
 fn get_pat_status(state: State<Mutex<PATState>>) -> PatStatus {
     let pat_state = state.lock().unwrap();
 
-    if pat_state.pat.len() > 0 {
+    let password = pat_state.pat_entry.get_password();
+    if password.is_ok() {
         PatStatus::Set
     } else {
         PatStatus::NotSet
@@ -28,8 +38,12 @@ fn get_pat_status(state: State<Mutex<PATState>>) -> PatStatus {
 
 #[tauri::command]
 fn set_pat(state: State<'_, Mutex<PATState>>, pat: String) {
-    let mut pat_state = state.lock().unwrap();
-    pat_state.pat = pat;
+    let pat_state = state.lock().unwrap();
+    if pat.len() > 0 {
+        pat_state.pat_entry.set_password(&pat).expect("set_password failed");
+    } else {
+        pat_state.pat_entry.delete_credential().expect("delete_credntial failed");
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
