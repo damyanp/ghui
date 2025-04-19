4f7f0181b8e4d08f4629f6ef7b30312eda3de5bb
Damyan Pepper <damyanp@gmail.com>
2025-04-19 15:22:49 -0700

    stylin

--- app/src/components/Pat.svelte
+++ app/src/components/Pat.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { Avatar } from "@skeletonlabs/skeleton-svelte";
+  import { TriangleAlert } from "@lucide/svelte";
+  import { Avatar, Popover } from "@skeletonlabs/skeleton-svelte";
   import { invoke } from "@tauri-apps/api/core";
   import { listen } from "@tauri-apps/api/event";
   import { onDestroy } from "svelte";
@@ -7,7 +8,7 @@
   let color = $state("secondary");
   let text = $state("...");
   let hasPat = $state(false);
-  let isOpen = $state(false);
+  let isOpen = $state(true);
   let avatar = $state<string | undefined>(undefined);
 
   type Status =
@@ -75,41 +76,58 @@
   }
 </script>
 
-<Avatar src={avatar} name={text} size="size-12"/>
-
-<!-- 
 <Popover
-  bind:isOpen={() => isOpen, setIsOpen}
-  hideOnOutsideClick
-  container="body"
-  target={buttonId}
-  placement="auto"
-  trigger="click"
-  title="Personal Access Token"
-  style="max-width:30em; min-width:40em"
+  open={isOpen}
+  onOpenChange={(e) => (isOpen = e.open)}
+  positioning={{ placement: "bottom" }}
+  contentBase="card bg-primary-50-950 p-4 space-y-4 max-w-[640px]"
+  arrow
+  arrowBackground="!bg-primary-50 dark:!bg-primary-950"
 >
-  <p>
-    This app needs access to github. Although we'd do a proper github login
-    flow, the llvm project requires github apps to be installed, which isn't
-    great for experiments. Instead we use a PAT. This means that as far as
-    github is concerned, this app is you. When you generate the PAT you can
-    control what it has access to.
-  </p>
+  {#snippet trigger()}
+    <Avatar src={avatar} name={text} size="size-12" fallbackBase="bg-error-500">
+      <div class="w-full h-full flex items-center justify-center">
+        <TriangleAlert class="text-error-500" size={32} />
+      </div>
+    </Avatar>
+  {/snippet}
+  {#snippet content()}
+    <header>
+      <p class="font-bold text-xl">Personal Access Token</p>
+    </header>
+    <article>
+      <p class="m-4">
+        This app needs access to github. Although we'd do a proper github login
+        flow, the llvm project requires github apps to be installed, which isn't
+        great for experiments. Instead we use a PAT. This means that as far as
+        github is concerned, this app is you. When you generate the PAT you can
+        control what it has access to.
+      </p>
 
-  <p>
-    <a target="_blank" href="https://github.com/settings/tokens"
-      >Generate PAT here</a
-    >. Tokens (classic) with access to projects seem to work well.
-  </p>
+      <p class="m-4">
+        <a target="_blank" href="https://github.com/settings/tokens"
+          >Generate PAT here</a
+        >. Tokens (classic) with access to projects seem to work well.
+      </p>
 
-  <Form>
-    <FormGroup floating label="Paste PAT here">
-      <Input type="password" bind:value={pat} />
-    </FormGroup>
-
-    <Button color="primary" disabled={pat.length == 0} on:click={setPat}
-      >Set</Button
-    >
-    <Button color="danger" disabled={!hasPat} on:click={clearPat}>Clear</Button>
-  </Form>
-</Popover> -->
+      <form class="w-full space-y-4 space-x-1">
+        <input
+          type="password"
+          class="input border-primary-500 bg-primary-100-900"
+          placeholder="Paste PAT here"
+          bind:value={pat}
+        />
+        <button
+          class="btn preset-filled-success-500"
+          disabled={pat.length == 0}
+          onclick={setPat}>Set</button
+        >
+        <button
+          class="btn preset-filled-warning-500"
+          disabled={!hasPat}
+          onclick={clearPat}>Clear</button
+        >
+      </form>
+    </article>
+  {/snippet}
+</Popover>
