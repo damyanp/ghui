e33cd5e95aa6a7ccaf0cc6e99278eeaa65379a43
Damyan Pepper <damyanp@gmail.com>
2025-05-18 20:26:15 -0700

    UI in place for mutator

--- app/src/components/WorkItemTree.svelte
+++ app/src/components/WorkItemTree.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
-  import { CircleMinusIcon, CirclePlusIcon } from "@lucide/svelte";
+  import {
+    BookOpenIcon,
+    CircleMinusIcon,
+    CirclePlusIcon,
+  } from "@lucide/svelte";
   import { fade } from "svelte/transition";
   import { flip } from "svelte/animate";
   import type { Data } from "$lib/bindings/Data";
   import type { Node } from "$lib/bindings/Node";
+  import * as floating from "@floating-ui/dom";
+  import type { WorkItem } from "$lib/bindings/WorkItem";
+  import { tick } from "svelte";
+  import { text } from "@sveltejs/kit";
 
   let { raw_data }: { raw_data: Data } = $props();
 
@@ -28,6 +36,67 @@
 
     return { ...raw_data, rootNodes: nodes };
   });
+
+  type MenuOption =
+    | { type: "text"; title: string }
+    | { type: "action"; title: string; action: () => void };
+
+  let menu_options = $state<MenuOption[]>([]);
+
+  function showMenu(e: MouseEvent, item: WorkItem) {
+    if (!e.currentTarget) return;
+
+    const menu = document.getElementById("menu") as HTMLDialogElement;
+    if (!menu) return;
+
+    const target = e.currentTarget as Element;
+    if (!target) return;
+
+    menu_options = [];
+
+    if (item.data.type === "issue") {
+      if (item.data.trackedIssues.length > 0) {
+        menu_options.push({
+          type: "action",
+          title: `Convert ${item.data.trackedIssues.length} tracked issues to sub-issues`,
+          action: () => convertTrackedIssuesToSubIssue(item),
+        });
+      }
+    }
+
+    if (menu_options.length === 0)
+      menu_options = [{ type: "text", title: "No actions" }];
+
+    // Use tick to ensure that the menu has figured out its layout before we
+    // attempt to figure out where to position it.
+    tick().then(() => {
+      floating
+        .computePosition(target, menu, {
+          placement: "bottom-start",
+          middleware: [
+            floating.offset(6),
+            floating.flip(),
+            floating.shift({ padding: 5 }),
+          ],
+        })
+        .then(({ x, y, placement }) => {
+          Object.assign(menu.style, { left: `${x}px`, top: `${y}px` });
+        });
+
+      menu.showModal();
+    });
+  }
+
+  function handleMenuSelect(option: MenuOption) {
+    const menu = document.getElementById("menu") as HTMLDialogElement;
+    menu.close();
+
+    if (option.type === "action") option.action();
+  }
+
+  function convertTrackedIssuesToSubIssue(item: WorkItem) {
+    // what now?
+  }
 </script>
 
 {@render itemList(data.rootNodes)}
@@ -72,7 +141,11 @@
                 <td>{item.projectItem.iteration?.title}</td>
                 <td>{item.projectItem.blocked?.name}</td>
                 <td>{item.projectItem.kind?.name}</td>
-                <td>{item.data.type === "issue" ? item.data.trackedIssues.length : ''}</td>
+                <td onclick={(e) => showMenu(e, item)}
+                  >{item.data.type === "issue"
+                    ? item.data.trackedIssues.length
+                    : ""}</td
+                >
               {/if}
             {/if}
           </tr>
@@ -103,6 +176,21 @@
     <div class="inline-block size-[1em]">&nbsp;</div>
   {/if}
 {/snippet}
+
+<dialog id="menu" closedby="any" class="absolute border p-2">
+  {#each menu_options as option}
+    {#if option.type === "text"}
+      <button class="p-2" onclick={() => handleMenuSelect(option)}>
+        {option.title}
+      </button>
+    {:else if option.type === "action"}
+      <button
+        class="btn bg-primary-500 p-2"
+        onclick={() => handleMenuSelect(option)}>{option.title}</button
+      >
+    {/if}
+  {/each}
+</dialog>
 
 <!-- <pre>{JSON.stringify(expanded, null, " ")}</pre> -->
 <!-- <pre>{JSON.stringify(result, null, " ")}</pre> -->
