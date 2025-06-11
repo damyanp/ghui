1b8c9a08f8b8178f4b39c03fb30e65c590594716
Damyan Pepper <damyanp@gmail.com>
2025-06-11 14:35:44 -0700

    wip

--- app/src/components/ExecutionTracker.svelte
+++ app/src/components/ExecutionTracker.svelte
@@ -1,5 +1,52 @@
-<script lang="ts">
+<script lang="ts" module>
+  export type BarState =
+    | "completed"
+    | "onTrack"
+    | "atRisk"
+    | "offTrack"
+    | "notStarted"
+    | "noDates";
 
+  export type Date = string;
+
+  export type Bar = {
+    state: BarState;
+    label?: string;
+    start?: Date;
+    end?: Date;
+  };
+
+  export type Epic = {
+    name: string;
+    targetDate: Date;
+    scenarios: Scenario[];
+  };
+
+  export type Scenario = {
+    name: string;
+    deliverables: Deliverable[];
+  };
+
+  export type Deliverable = {
+    name: string;
+    bars: Bar[];
+  };
+
+  export type Data = {
+    epics: Epic[]
+  };
 </script>
 
-<h1>Execution Tracker</h1>
+<script lang="ts">
+  let { data }: { data: Data } = $props();
+</script>
+
+
+{#each data.epics as epic}
+{epic.name} / {epic.targetDate}
+
+<svg class="border">
+    <circle cx="50" cy="50" r="40" />
+</svg>
+{/each}
+
--- app/src/routes/test_xt/+page.svelte
+++ app/src/routes/test_xt/+page.svelte
@@ -4,12 +4,64 @@
     needing a full tauri stack running.
     */
 
-  import ExecutionTracker from "../../components/ExecutionTracker.svelte";
+  import ExecutionTracker, { type Data } from "../../components/ExecutionTracker.svelte";
+
+  const data : Data = {
+    epics: [
+      {
+        name: "DML Demo",
+        targetDate: "2025-12-31",
+        scenarios: [
+          {
+            name: "Intrinsics Functional Tests",
+            deliverables: [
+              {
+                name: "Intrinsics Functional Tests Scenario Planning",
+                bars: [
+                  {
+                    state: "completed",
+                    start: "2025-04-07",
+                    end: "2025-04-27",
+                  },
+                ],
+              },
+              {
+                name: "Update Testing Infrastructure",
+                bars: [
+                  {
+                    label: "planning",
+                    state: "completed",
+                    start: "2025-04-14",
+                    end: "2025-04-29",
+                  },
+                  {
+                    label: "active",
+                    state: "noDates",
+                  },
+                ],
+              },
+              {
+                name: "Implement Intrinsic tests in offload test suite",
+                bars: [
+                  {
+                    label: "planning",
+                    state: "completed",
+                    start: "2025-04-29",
+                    end: "2025-05-25",
+                  },
+                  { label: "active", state: "onTrack", end: "2025-08-31" },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
 </script>
 
 <div class="flex justify-center items-center h-[100vh]">
   <div class="border min-w-[75vw] min-h-[75vh]">
-    <ExecutionTracker />
+    <ExecutionTracker data={data}/>
   </div>
 </div>
-
