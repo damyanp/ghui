bdd27beb38697cfeb1b6138d299a3e5d456648dc
Damyan Pepper <damyanp@gmail.com>
2025-06-11 13:22:44 -0700

    Add test route for execution tracker

---
+++ app/src/routes/test_xt/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  /*
+    Use "npm run dev" and then browse to localhost/test_xt to test this without
+    needing a full tauri stack running.
+    */
+
+  import ExecutionTracker from "../../components/ExecutionTracker.svelte";
+</script>
+
+<div class="flex justify-center items-center h-[100vh]">
+  <div class="border min-w-[75vw] min-h-[75vh]">
+    <ExecutionTracker />
+  </div>
+</div>
+
