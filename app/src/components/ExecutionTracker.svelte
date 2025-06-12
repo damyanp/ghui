e3761492b1c0c543a8701ebf4dfa78bf3c5f4018
Damyan Pepper <damyanp@gmail.com>
2025-06-11 21:00:52 -0700

    wip: data from last execution tracking mail

--- app/src/components/ExecutionTracker.svelte
+++ app/src/components/ExecutionTracker.svelte
@@ -24,16 +24,15 @@
 
   export type Scenario = {
     name: string;
-    deliverables: Deliverable[];
+    rows: Row[];
   };
 
-  export type Deliverable = {
-    name: string;
+  export type Row = {
     bars: Bar[];
   };
 
   export type Data = {
-    epics: Epic[]
+    epics: Epic[];
   };
 </script>
 
@@ -41,12 +40,35 @@
   let { data }: { data: Data } = $props();
 </script>
 
+<div class="overflow-x-auto">
+  <div class="grid gap-1" style="grid-template-columns: auto auto auto 1fr ">
+    
+    <div>Product Epic</div>
+    <div>Target Date</div>
+    <div>Engineering Scenarios</div>
+    <div>&nbsp;</div>
 
-{#each data.epics as epic}
-{epic.name} / {epic.targetDate}
+    {#each data.epics as epic}
+      <div>{epic.name}</div>
+      <div>{epic.targetDate}</div>
+      <div class="col-start-3 col-end-5 grid grid-cols-subgrid gap-1">
+        {#each epic.scenarios as scenario}
+          <div class="">{scenario.name}</div>
+          <div class="col-start-5 col-end-6 grid-cols-subgrid w-full">
+            {#each scenario.rows as row}
+            <div>
+                {#each row.bars as bar}
+                <div class="p-x-2 bg-amber-950 w-fit mx-2 inline">{bar.label}&nbsp;</div>
+                {/each}
+            </div>
+            {/each}
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
+</div>
 
-<svg class="border">
-    <circle cx="50" cy="50" r="40" />
-</svg>
-{/each}
-
+<style lang="postcss">
+    @reference "../app.css";
+</style>
--- app/src/routes/test_xt/+page.svelte
+++ app/src/routes/test_xt/+page.svelte
@@ -4,52 +4,268 @@
     needing a full tauri stack running.
     */
 
-  import ExecutionTracker, { type Data } from "../../components/ExecutionTracker.svelte";
+  import ExecutionTracker, {
+    type Data,
+  } from "../../components/ExecutionTracker.svelte";
 
-  const data : Data = {
+  const data: Data = {
     epics: [
+      {
+        name: "Particle Life",
+        targetDate: "2025-04-31",
+        scenarios: [
+          {
+            name: "Constant Buffers",
+            rows: [
+              {
+                bars: [
+                  {
+                    state: "completed",
+                    label: "Implementation",
+                    start: "2025-01-12",
+                    end: "2025-04-06",
+                  },
+                  {
+                    state: "noDates",
+                    end: "2025-04-20",
+                  },
+                ],
+              },
+            ],
+          },
+
+          {
+            name: "Particle Life Finalization",
+            rows: [
+              {
+                bars: [
+                  {
+                    state: "completed",
+                    start: "2025-04-19",
+                    end: "2025-05-04",
+                    label: "0 of 2 Bugs Resolved",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
       {
         name: "DML Demo",
         targetDate: "2025-12-31",
         scenarios: [
           {
-            name: "Intrinsics Functional Tests",
-            deliverables: [
+            name: "Testing Infra",
+            rows: [
               {
-                name: "Intrinsics Functional Tests Scenario Planning",
                 bars: [
                   {
                     state: "completed",
-                    start: "2025-04-07",
-                    end: "2025-04-27",
+                    start: "2025-05-03",
+                    end: "2025-05-16",
+                    label: "Implementation",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Language: Casting/Overload",
+            rows: [
+              {
+                bars: [
+                  {
+                    state: "completed",
+                    start: "2025-03-17",
+                    end: "2025-04-06",
+                    label: "Implementation",
+                  },
+                  { state: "noDates", end: "2025-04-27" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Implicit Resource Bindings",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Requirements/Test Cases",
+                    state: "completed",
+                    start: "2025-03-17",
+                    end: "2025-03-30",
+                  },
+                  {
+                    label: "Design and Planning",
+                    state: "completed",
+                    end: "2025-04-13",
+                  },
+                  {
+                    label: "Implementation by June 1st",
+                    state: "completed",
+                    end: "2025-06-01",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Root Signatures",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Requirements/Test Cases/Design",
+                    state: "completed",
+                    start: "2025-03-17",
+                    end: "2025-04-06",
                   },
                 ],
               },
               {
-                name: "Update Testing Infrastructure",
                 bars: [
                   {
-                    label: "planning",
+                    label: "Implementation by June 29th",
+                    state: "onTrack",
+                    start: "2025-03-31",
+                    end: "2025-06-29",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Shader Flags",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Requirements/Test Cases",
                     state: "completed",
-                    start: "2025-04-14",
-                    end: "2025-04-29",
+                    start: "2025-03-17",
+                    end: "2025-03-30",
+                  },
+                  { label: "Planning", state: "completed", end: "2025-04-06" },
+                  {
+                    label: "Implementation",
+                    state: "completed",
+                    end: "2025-04-20",
+                  },
+                  { state: "noDates", end: "2025-04-27" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "ML Preview Exec Text",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Requirements/Planning",
+                    state: "completed",
+                    start: "2025-03-24",
+                    end: "2025-03-30",
                   },
                   {
-                    label: "active",
+                    label: "Implementation",
+                    state: "completed",
+                    end: "2025-04-20",
+                  },
+                  { state: "noDates", end: "2025-04-27" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Intrinsics Functional Tests",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Scenario Planning",
+                    state: "completed",
+                    start: "2025-04-07",
+                    end: "2025-04-13",
+                  },
+                  {
+                    label: "Update Testing Infrastructure",
+                    state: "completed",
+                    end: "2025-04-27",
+                  },
+                  { state: "noDates", end: "2025-06-01" },
+                ],
+              },
+              {
+                bars: [
+                  {
+                    label: "Implementation Planning",
+                    state: "completed",
+                    start: "2025-04-27",
+                    end: "2025-05-25",
+                  },
+                  {
+                    label: "Implementation by Aug 31st",
+                    state: "onTrack",
+                    end: "2025-08-31",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Legalization and Lowering",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "8 of 17 Bugs Resolved, 4 Active",
+                    state: "onTrack",
+                    start: "2025-04-28",
+                    end: "2025-06-01",
+                  },
+                ],
+              },
+              {
+                bars: [
+                  {
+                    label: "Planning Symbol Visibility",
+                    state: "noDates",
+                    start: "2025-05-18",
+                    end: "2025-06-01",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Buffer Resources",
+            rows: [
+              {
+                bars: [
+                  {
+                    label: "Scenario Planning",
+                    start: "2025-04-28",
+                    end: "2025-06-01",
                     state: "noDates",
                   },
                 ],
               },
+            ],
+          },
+          {
+            name: "ML Demo Finalization",
+            rows: [
               {
-                name: "Implement Intrinsic tests in offload test suite",
                 bars: [
                   {
-                    label: "planning",
-                    state: "completed",
-                    start: "2025-04-29",
-                    end: "2025-05-25",
+                    label: "3 of 8 Bugs Resolved, 1 Active",
+                    state: "onTrack",
+                    start: "2025-04-28",
+                    end: "2025-06-01",
                   },
-                  { label: "active", state: "onTrack", end: "2025-08-31" },
                 ],
               },
             ],
@@ -62,6 +278,6 @@
 
 <div class="flex justify-center items-center h-[100vh]">
   <div class="border min-w-[75vw] min-h-[75vh]">
-    <ExecutionTracker data={data}/>
+    <ExecutionTracker {data} />
   </div>
 </div>
